b544377cf6b6287e210da1a6f544f6ae00122842
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2024-04-21 01:08:19 +0700

    init

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tranhuuhuy297/simple-bank-golang/util"
+)
+
+func createRandomAccount() (Account, error) {
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+
+	return account, err
+}
+
+func TestCreateAccount(t *testing.T) {
+	account, err := createRandomAccount()
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1, _ := createRandomAccount()
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+}
+
+func TestUpdateAccount(t *testing.T) {
+
+}
+
+func TestDeleteAccount(t *testing.T) {
+
+}
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://admin:admin@localhost:5432/postgres?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := pgx.Connect(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
