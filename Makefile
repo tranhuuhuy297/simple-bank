d9a66a8740e5230f272282e228096e1845bf696e
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2024-04-24 14:31:08 +0700

    add user

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
-DB_URL=postgresql://admin:admin@localhost:5432/simple_bank?sslmode=disable
+DB_URL=postgresql://admin:admin@localhost:5432/postgres?sslmode=disable
 
 migrate-up:
 	migrate -path db/migration -database "$(DB_URL)" -verbose up
+migrate-down:
+	migrate -path db/migration -database "$(DB_URL)" -verbose down
 run-test:
 	go test -v ./...
 run-server:
--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/tranhuuhuy297/simple-bank-golang/db/sqlc"
 )
 
@@ -14,10 +16,16 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	// accounts
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.getListAccount)
+	//transfer
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 
